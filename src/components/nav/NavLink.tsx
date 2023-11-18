76c3c8165e0056a22686a62591d51f7b81344664
jsabram <js.abramczyk@gmail.com>
2023-11-18 19:57:35 +0100

    Added CloseIcon and close IconButton to NavLinks

--- src/components/nav/NavLink.tsx
+++ src/components/nav/NavLink.tsx
@@ -1,7 +1,13 @@
 import { LinkProps } from '../../@types/component-types';
 import DocumentIcon from '../../assets/icons/icon-components/DocumentIcon';
+import IconButton from '../reusable/IconButton';
+import CloseIcon from '../../assets/icons/icon-components/CloseIcon';
 
-const NavLink: React.FC<LinkProps> = ({ id, title, docName }) => {
+const NavLink: React.FC<LinkProps> = ({ id, docName }) => {
+	const closeTabHandler = () => {
+		console.log('Tab closed');
+	};
+
 	return (
 		<li className='link-element md:border-r-[1px] md:border-textGray200 lg:px-5'>
 			<a href={id} className='document-link outline-none'>
@@ -11,6 +17,9 @@ const NavLink: React.FC<LinkProps> = ({ id, title, docName }) => {
 				<h3 className='hidden lg:block'>Document name</h3>
 				<p>{docName}</p>
 			</div>
+			<IconButton className='ms-3' onClick={closeTabHandler}>
+				<CloseIcon />
+			</IconButton>
 		</li>
 	);
 };
