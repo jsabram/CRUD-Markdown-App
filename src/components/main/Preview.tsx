40c947d398e01be5aa59e504cb0bb7d03d343ae8
jsabram <js.abramczyk@gmail.com>
2023-11-28 22:54:38 +0100

    Refactored Preview component, styled extended Markdown components

--- src/components/main/Preview.tsx
+++ src/components/main/Preview.tsx
@@ -1,14 +1,43 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { useAppSelector } from '../../store/typed-hooks';
 import { AppContext } from '../../context/AppContext';
 import SectionHeader from '../reusable/SectionHeader';
-import Markdown from 'react-markdown';
+import { unified } from 'unified';
+import {
+	remarkDefinitionList,
+	defListHastHandlers,
+} from 'remark-definition-list';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import remarkGfm from 'remark-gfm';
+import rehypeStringify from 'rehype-stringify';
 
 const Preview = () => {
 	const { selectedView } = useContext(AppContext);
 
 	const markdown = useAppSelector((state) => state.editorValue);
-	
+
+	const compileHandler = async (md: any) => {
+		const html = await unified()
+			.use(remarkParse)
+			.use(remarkGfm)
+			.use(remarkDefinitionList)
+			.use(remarkRehype, {
+				handlers: {
+					...defListHastHandlers,
+				},
+			})
+			.use(rehypeStringify)
+			.process(md);
+
+		console.log(html.value);
+		document.querySelector('#preview')!.innerHTML = html.value.toString();
+	};
+
+	useEffect(() => {
+		compileHandler(markdown);
+	}, [markdown]);
+
 	return (
 		<section
 			className={`relative h-full bg-white dark:bg-darkGray500 ${
@@ -16,9 +45,10 @@ const Preview = () => {
 			}`}
 		>
 			<SectionHeader sectionTitle='Preview' />
-			<Markdown className='preview h-full px-8 pt-10 pb-2 text-darkGray200 font-preview overflow-y-scroll dark:text-textGray100'>
-				{markdown}
-			</Markdown>
+			<div
+				id='preview'
+				className='preview h-full px-8 pt-10 pb-2 text-darkGray200 font-preview overflow-y-scroll dark:text-textGray100'
+			></div>
 		</section>
 	);
 };
